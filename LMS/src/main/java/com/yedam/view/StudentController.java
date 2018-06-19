57d972d5fd7c8fee31e52ee95e06712541f2d7d3
girrrMaster <knm487487@naver.com>
2018-06-19 17:32:41 +0900

    AAAA

--- LMS/src/main/java/com/yedam/view/StudentController.java
+++ LMS/src/main/java/com/yedam/view/StudentController.java
@@ -1,5 +1,26 @@
 package com.yedam.view;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.yedam.smp.StudentService;
+import com.yedam.smp.StudentVO;
+import com.yedam.smp.impl.StudentDAO;
+
+@Controller
+@SessionAttributes("student")
 public class StudentController {
 
+	@Autowired
+	StudentService studentService;
+	
+	@RequestMapping("/getStudentList")
+	public String getStudentList(HttpServletRequest request, StudentVO vo) {
+		//request.setAttribute("serList", StudentDAO.());
+		return "student/getStudentList";
+	}
 }
