44701f2ba3b23b8d0ed5e34a1331a0456e026f44
girrrMaster <knm487487@naver.com>
2018-06-19 17:52:14 +0900

    ㄴㄴㄴㄴ

--- LMS/src/main/java/com/yedam/view/StudentController.java
+++ LMS/src/main/java/com/yedam/view/StudentController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.yedam.smp.StudentService;
@@ -20,7 +21,13 @@ public class StudentController {
 	
 	@RequestMapping("/getStudentList")
 	public String getStudentList(HttpServletRequest request, StudentVO vo) {
-		//request.setAttribute("serList", StudentDAO.());
+		request.setAttribute("serList", studentService.getStudentList());
 		return "student/getStudentList";
 	}
+	//등록폼
+	@RequestMapping(value="/insertStudnet",method=RequestMethod.GET)
+	String insertStudentForm() {
+		return "student/insertStudent";
+	}
+	
 }
